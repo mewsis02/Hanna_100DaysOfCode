be3598b8b3f5e7c6f391ad6c05678fbf03d4c4c5
Hanna <mewsis02@gmail.com>
2024-08-18 21:39:35 -0400

    Changed the number to be guessed from 1-100 to 1-500, and fixed the logic of the if/elif.

--- Day 12/Number Guessing Project/main.py
+++ Day 12/Number Guessing Project/main.py
@@ -4,24 +4,24 @@ import art, random
 print(art.logo)
 
 print("Welcome To Hanna Guesses!")
-print("In this game, You will guess a number between 1 and 100,")
+print("In this game, You will guess a number between 1 and 500,")
 difficulty = input("but first, would you like to play easy, normal or hard? ").lower()
 starting_num_of_guesses = 0
 
 if difficulty == "easy":
-    starting_num_of_guesses = 9
+    starting_num_of_guesses = 10
 elif difficulty == "normal":
-    starting_num_of_guesses = 7
+    starting_num_of_guesses = 8
 elif difficulty == "hard":
-    starting_num_of_guesses = 5
+    starting_num_of_guesses = 6
 else:
-    starting_num_of_guesses = 9
+    starting_num_of_guesses = 10
     print("I will assume you want to play easy mode.")
 print(f"You get {starting_num_of_guesses} attempts to mess around with!")
 
 keepGuessing = True
 lowest_number = 1
-highest_number = 100
+highest_number = 500
 answer = random.randint(lowest_number, highest_number)
 num_of_guesses = starting_num_of_guesses
 
@@ -30,7 +30,13 @@ while keepGuessing:
     print(f"You have {num_of_guesses} attempts to guess the random number.")
     guess = int(input(f"Please give me a guess between {lowest_number}-{highest_number}: "))
     num_of_guesses -= 1
-    if guess > answer:
+    if guess == answer:
+        print(f"You guessed the answer of {answer} in {starting_num_of_guesses - num_of_guesses} attempts!")
+        keepGuessing = False
+    elif num_of_guesses == 0:
+        print(f"The correct answer was {answer}.")
+        keepGuessing = False
+    elif guess > answer:
         print("Too high. Please guess again.")
         if not guess > highest_number:
             highest_number = guess
@@ -38,9 +44,3 @@ while keepGuessing:
         print("Too low. Please guess again.")
         if not guess < lowest_number:
             lowest_number = guess
-    elif guess == answer:
-        print(f"You guessed the answer of {answer} in {starting_num_of_guesses - num_of_guesses} attempts!")
-        keepGuessing = False
-    elif num_of_guesses == 0:
-        print(f"The correct answer was {answer}.")
-        keepGuessing = False
