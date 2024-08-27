357561765aee3e8b29d34cbe223ed4f0756d063c
Hanna <mewsis02@gmail.com>
2024-08-26 20:35:16 -0400

    Completed Day 16 Lessons!

---
+++ Day 16/main.py
@@ -0,0 +1,27 @@
+# from turtle import Turtle, Screen
+#
+# little_tim = Turtle()
+# print(little_tim)
+# little_tim.shape("turtle")
+# little_tim.color("LightPink1")
+# little_tim.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# print(my_screen.canvwidth)
+# my_screen.exitonclick()
+
+# Creates a new PrettyTable
+from prettytable import PrettyTable
+table = PrettyTable()
+
+pokemon_names = ["Pikachu", "Squirtle", "Charmander"]
+pokemon_types = ["Electric", "Water", "Fire"]
+# Adds 2 columns, named Pokémon Name and Type, to the PrettyTable
+table.add_column("Pokemon Name", pokemon_names)
+table.add_column("Type", pokemon_types)
+
+# Aligns all the columns in our table to be (left, right, or center) aligned
+table.align = "l"
+
+print(table)
